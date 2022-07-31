ad70372afb0801c46896f947e20fdbb7abd73b68
LuizHenri16 <lbastos722@gmail.com>
2022-07-31 09:36:53 -0300

    floorAnimation

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const pipe = document.querySelector(".pipe")
 const clouds = document.querySelector(".clouds")
 const clouds2 = document.querySelector(".clouds2")
 const gameOverText = document.querySelector(".game-over")
+const gameDisplay = document.querySelector(".Game-Display")
+const floor = document.querySelector(".floor")
+const floor2 = document.querySelector(".floor2")
 
 function Jump() {
   mario.classList.add("Jump")
@@ -25,12 +28,16 @@ window.addEventListener('keydown', function(event) {
   }
 })
 
+const loop = setInterval( ()=> {
 
-
-const loop = setInterval(()=>{
   const pipePosition = pipe.offsetLeft;
+
   const cloudsPosition = clouds.offsetLeft
   const clouds2Position = clouds2.offsetLeft
+
+  const floorPosition = floor.offsetLeft
+  const floor2Position = floor2.offsetLeft
+
   const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', "");
 
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
@@ -47,20 +54,22 @@ const loop = setInterval(()=>{
 
     clouds.style.animation = "none"
     clouds2.style.animation = "none"
+    floor.style.animation = "none"
+    floor2.style.animation = "none"
 
     clouds.style.left = `${cloudsPosition}px`
     clouds2.style.left = `${clouds2Position}px`
+    floor.style.left = `${floorPosition}px`
+    floor2.style.left = `${floor2Position}px`
+    
+
     gameOverText.classList.add("on")
 
+    gameDisplay.style.animation = "none"
 
     clearInterval(loop)
-
   }
-
 }, 10)
-
-
-
 
 function restart() {
   document.location.reload()
